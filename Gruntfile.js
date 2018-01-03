34b5b3df6ca1edf7335720f612195408ecf7c087
mpetty <mipett23@gmail.com>
2018-01-03 14:53:12 -0500

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,79 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		concat: {
+			dist: {
+				src: [
+					'src/jquery.conditional.js'
+				],
+				dest: 'jquery.conditional.min.js',
+			}
+		},
+
+		uglify: {
+			dist: {
+				files: {
+					'jquery.conditional.min.js' : [
+						'jquery.conditional.min.js'
+					]
+				}
+			}
+		},
+
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'src/*.js'
+			],
+			options: {
+				curly: false,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				boss: true,
+				eqnull: true,
+				browser: true,
+				jquery: true,
+
+				globals: {
+					module: true,
+					require: true,
+					jQuery: true,
+					console: true,
+					define: true
+				}
+			}
+		},
+
+		watch: {
+			js: {
+				files: [
+					'src/*.js'
+				],
+				tasks: [
+					'jshint',
+					'concat',
+					'uglify'
+				]
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', [
+		'concat',
+		'uglify'
+	]);
+
+};
---
+++ README.md
@@ -0,0 +1,31 @@
+# Conditional v0.1
+
+Library for conditionally displaying html based on field values.
+
+- Requires jQuery 1.7+
+
+## Bower Installation
+
+Add to your project's `bower.json` file, like:
+
+```json
+{
+  "name": "my-project",
+  "version": "1.0.0",
+  "dependencies": {
+    "jquery": "1.11.0",
+    "modal": "git@github.com:mpetty/conditional"
+  }
+}
+```
+
+## Usage
+
+```javascript
+$('form').conditional(options);
+```
+
+## Options available
+
+```javascript
+```
